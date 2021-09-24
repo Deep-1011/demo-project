d0f8181b59da906ffaaa4b6c599d7a06c4e115fe
Deep-1011 <deepshikhapardeshi@gmail.com>
2021-09-24 14:41:30 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'flower-market-project';
+  title = 'Calculator App';
+  num1!: number;
+  num2!: number;
+  result!: number;
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+  substract() {
+    this.result = this.num1 - this.num2;
+  }
+  multiply() {
+    this.result = this.num1 * this.num2;
+  }
+  divide() {
+    this.result = this.num1 % this.num2;
+  }
 }
